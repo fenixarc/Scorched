0542c40c650eb6719f8f9bf7ff391f5787ac8cf4
fenix <fenix@LAPTOP-ST7UDM3H>
2026-06-28 23:17:09 -0500

    Initial commit of local project

---
+++ src/main/java/EnvironmentPalette.java
@@ -0,0 +1,14 @@
+import java.awt.Color;
+
+/**
+ * Simple container to bind a matching sky and dirt color profile together.
+ */
+class EnvironmentPalette {
+	Color sky;
+	Color dirt;
+
+	public EnvironmentPalette(Color sky, Color dirt) {
+		this.sky = sky;
+		this.dirt = dirt;
+	}
+}
---
+++ src/main/java/GameWindow.java
@@ -0,0 +1,38 @@
+import javax.swing.JFrame;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+public class GameWindow {
+
+	public static void main(String[] args) {
+		JFrame window = new JFrame();
+
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setTitle("Scorched");
+
+		// Fullscreen borderless
+		window.setUndecorated(true);
+
+		// Get screen dimensions
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		int screenWidth = (int) screenSize.getWidth();
+		int screenHeight = (int) screenSize.getHeight();
+
+		// Setup gameEngine
+		GameEngine gameEngine = new GameEngine(screenWidth, screenHeight);
+		window.add(gameEngine);
+
+		// Fit the window
+		window.pack();
+
+		// Lock window to top-left corner and maximize across screen
+		window.setLocationRelativeTo(null);
+		window.setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+		window.setVisible(true);
+
+		System.out.println("Game window created");
+
+		gameEngine.startGameLoop();
+	}
+}
