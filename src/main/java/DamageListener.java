354765ff5412b09a094ff0f8393b983db3154d74
Kevin <fenixarc@hotmail.com>
2026-06-29 15:20:01 -0500

    Hide/Remove files

--- src/main/java/DamageListener.java
+++
@@ -1,8 +0,0 @@
-public interface DamageListener {
-	
-    // Fires when a tank is hit
-    void onTankTakeDamage(int x, int y, int amount);
-    
-    // Fires when a tank dies
-    void onTurretSpawned(TurretDebris debris);
-}
